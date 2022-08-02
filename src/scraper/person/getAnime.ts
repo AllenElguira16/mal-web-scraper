5a6b3320e6ce357a95bf99ab8c3a84de908df1fb
Allen Elguira <AllenElguira16@gmail.com>
2022-08-02 19:08:37 +0800

    update: add pictures to have nullable instead of error

--- src/scraper/person/getAnime.ts
+++ src/scraper/person/getAnime.ts
@@ -6,64 +6,67 @@ export const getAnime = async (page: Page) => {
     '//h2[contains(., "Voice Acting Roles")]'
   );
 
-  return vaH2roleElement.evaluate((vaH2roleNode) => {
-    const animeTableElement = (vaH2roleNode?.parentNode as Element | undefined)
-      ?.nextElementSibling?.nextElementSibling;
+  return (
+    vaH2roleElement?.evaluate((vaH2roleNode) => {
+      const animeTableElement = (
+        vaH2roleNode?.parentNode as Element | undefined
+      )?.nextElementSibling?.nextElementSibling;
 
-    const anime: AnimeOfPerson[] = [];
+      const anime: AnimeOfPerson[] = [];
 
-    if (animeTableElement?.tagName === "TABLE") {
-      animeTableElement
-        .querySelectorAll("tbody > tr")
-        .forEach((animeRowElement) => {
-          const animeLink = animeRowElement
-            .querySelector("td:nth-of-type(1) a")
-            ?.getAttribute("href") as string;
+      if (animeTableElement?.tagName === "TABLE") {
+        animeTableElement
+          .querySelectorAll("tbody > tr")
+          .forEach((animeRowElement) => {
+            const animeLink = animeRowElement
+              .querySelector("td:nth-of-type(1) a")
+              ?.getAttribute("href") as string;
 
-          const animePicture =
-            animeRowElement
-              .querySelector("td:nth-of-type(1) img")
-              ?.getAttribute("data-src") || null;
+            const animePicture =
+              animeRowElement
+                .querySelector("td:nth-of-type(1) img")
+                ?.getAttribute("data-src") || null;
 
-          const animeTitle = animeRowElement.querySelector(
-            "td:nth-of-type(2) > .spaceit_pad:nth-of-type(1) > a"
-          )?.textContent as string;
+            const animeTitle = animeRowElement.querySelector(
+              "td:nth-of-type(2) > .spaceit_pad:nth-of-type(1) > a"
+            )?.textContent as string;
 
-          const characterName = animeRowElement
-            .querySelector("td:nth-of-type(3) .spaceit_pad:nth-of-type(1) a")
-            ?.getAttribute("href") as string;
+            const characterName = animeRowElement
+              .querySelector("td:nth-of-type(3) .spaceit_pad:nth-of-type(1) a")
+              ?.getAttribute("href") as string;
 
-          const characterLink = animeRowElement.querySelector(
-            "td:nth-of-type(3) .spaceit_pad:nth-of-type(1) a"
-          )?.textContent as string;
+            const characterLink = animeRowElement.querySelector(
+              "td:nth-of-type(3) .spaceit_pad:nth-of-type(1) a"
+            )?.textContent as string;
 
-          const characterRole = animeRowElement.querySelector(
-            "td:nth-of-type(3) .spaceit_pad:nth-of-type(2)"
-          )?.textContent as string;
+            const characterRole = animeRowElement.querySelector(
+              "td:nth-of-type(3) .spaceit_pad:nth-of-type(2)"
+            )?.textContent as string;
 
-          const characterPicture =
-            animeRowElement
-              .querySelector("td:nth-of-type(4) img")
-              ?.getAttribute("data-src") || null;
+            const characterPicture =
+              animeRowElement
+                .querySelector("td:nth-of-type(4) img")
+                ?.getAttribute("data-src") || null;
 
-          anime.push({
-            anime_id: parseInt(
-              animeLink.split("/")[animeLink.split("/").length - 2]
-            ),
-            picture: animePicture,
-            role: characterRole.trim(),
-            title: animeTitle,
-            character: {
-              character_id: parseInt(
-                characterLink.split("/")[characterLink.split("/").length - 2]
+            anime.push({
+              anime_id: parseInt(
+                animeLink.split("/")[animeLink.split("/").length - 2]
               ),
-              name: characterName,
-              picture: characterPicture,
-            },
+              picture: animePicture,
+              role: characterRole.trim(),
+              title: animeTitle,
+              character: {
+                character_id: parseInt(
+                  characterLink.split("/")[characterLink.split("/").length - 2]
+                ),
+                name: characterName,
+                picture: characterPicture,
+              },
+            });
           });
-        });
-    }
+      }
 
-    return anime;
-  });
+      return anime;
+    }) || []
+  );
 };
--- src/scraper/person/getAnimeStaff.ts
+++ src/scraper/person/getAnimeStaff.ts
@@ -6,44 +6,46 @@ export const getAnimeStaff = async (page: Page) => {
     '//*[contains(text(), "Anime Staff Positions")]'
   );
 
-  return animeStaffH2roleElement.evaluate((animeStaffH2roleNode) => {
-    const animeStaffTableElement = (animeStaffH2roleNode as Element)
-      ?.nextElementSibling?.nextElementSibling;
+  return (
+    animeStaffH2roleElement?.evaluate((animeStaffH2roleNode) => {
+      const animeStaffTableElement = (animeStaffH2roleNode as Element)
+        ?.nextElementSibling?.nextElementSibling;
 
-    const staff: StaffOfPerson[] = [];
+      const staff: StaffOfPerson[] = [];
 
-    if (animeStaffTableElement?.tagName === "TABLE") {
-      animeStaffTableElement
-        .querySelectorAll("tbody > tr")
-        .forEach((animeRowElement) => {
-          const animeLink = animeRowElement
-            .querySelector("td:nth-of-type(1) a")
-            ?.getAttribute("href") as string;
+      if (animeStaffTableElement?.tagName === "TABLE") {
+        animeStaffTableElement
+          .querySelectorAll("tbody > tr")
+          .forEach((animeRowElement) => {
+            const animeLink = animeRowElement
+              .querySelector("td:nth-of-type(1) a")
+              ?.getAttribute("href") as string;
 
-          const animePicture =
-            animeRowElement
-              .querySelector("td:nth-of-type(1) img")
-              ?.getAttribute("data-src") || null;
+            const animePicture =
+              animeRowElement
+                .querySelector("td:nth-of-type(1) img")
+                ?.getAttribute("data-src") || null;
 
-          const animeTitle = animeRowElement.querySelector(
-            "td:nth-of-type(2) > .spaceit_pad:nth-of-type(1) > a"
-          )?.textContent as string;
+            const animeTitle = animeRowElement.querySelector(
+              "td:nth-of-type(2) > .spaceit_pad:nth-of-type(1) > a"
+            )?.textContent as string;
 
-          const staffPosition = animeRowElement.querySelector(
-            "td:nth-of-type(2) .spaceit_pad:nth-of-type(2) small"
-          )?.textContent as string;
+            const staffPosition = animeRowElement.querySelector(
+              "td:nth-of-type(2) .spaceit_pad:nth-of-type(2) small"
+            )?.textContent as string;
 
-          staff.push({
-            anime_id: parseInt(
-              animeLink.split("/")[animeLink.split("/").length - 2]
-            ),
-            picture: animePicture,
-            position: staffPosition.trim(),
-            title: animeTitle,
+            staff.push({
+              anime_id: parseInt(
+                animeLink.split("/")[animeLink.split("/").length - 2]
+              ),
+              picture: animePicture,
+              position: staffPosition.trim(),
+              title: animeTitle,
+            });
           });
-        });
-    }
+      }
 
-    return staff;
-  });
+      return staff;
+    }) || []
+  );
 };
