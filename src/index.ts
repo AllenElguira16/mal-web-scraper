f744c23ba0a23a690d04f5c686e08441e3b13867
Allen Elguira <AllenElguira16@gmail.com>
2022-08-09 12:59:31 +0800

    Restructure to only use single browser per transaction

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import { DEFAULT_INTERCEPT_RESOLUTION_PRIORITY } from "puppeteer";
+import {
+  Browser,
+  DEFAULT_INTERCEPT_RESOLUTION_PRIORITY,
+  Page,
+} from "puppeteer";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import AdblockerPlugin from "puppeteer-extra-plugin-adblocker";
@@ -8,45 +12,68 @@ import { minimal_args } from "./const";
 
 import { anime, character, person } from "./scraper";
 
-const MalWebScraper = async () => {
-  puppeteer.use(StealthPlugin());
-  puppeteer.use(
-    AdblockerPlugin({
-      interceptResolutionPriority: DEFAULT_INTERCEPT_RESOLUTION_PRIORITY,
-    })
-  );
+class MalWebScraper {
+  private static instance?: MalWebScraper;
+  private browser!: Browser;
+  private page!: Page;
 
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: minimal_args,
-    userDataDir: path.resolve(__dirname, "../../.cache"),
-  });
+  private constructor() {
+    puppeteer.use(StealthPlugin());
+    puppeteer.use(
+      AdblockerPlugin({
+        interceptResolutionPriority: DEFAULT_INTERCEPT_RESOLUTION_PRIORITY,
+      })
+    );
+  }
 
-  // Create a new incognito browser context.
-  const context = await browser.createIncognitoBrowserContext();
-  // Create a new page in a pristine context.
-  const page = await context.newPage();
+  public static async init() {
+    if (!this.instance) this.instance = new MalWebScraper();
 
-  const blocked_domains = ["googlesyndication.com", "adservice.google.com"];
+    if (!this.instance.browser || !this.instance.page) {
+      this.instance.browser = await puppeteer.launch({
+        headless: true,
+        args: minimal_args,
+        userDataDir: path.resolve(__dirname, "../../.cache"),
+      });
 
-  await page.setRequestInterception(true);
+      // Create a new incognito browser context.
+      const context =
+        await this.instance.browser.createIncognitoBrowserContext();
+      // Create a new page in a pristine context.
+      this.instance.page = await context.newPage();
 
-  page.on("request", (request) => {
-    const url = request.url();
-    if (blocked_domains.some((domain) => url.includes(domain))) request.abort();
-    else request.continue();
-  });
+      const blocked_domains = ["googlesyndication.com", "adservice.google.com"];
 
-  return {
-    anime: (id: number) => anime(page, id),
-    character: (id: number) => character(page, id),
-    person: (id: number) => person(page, id),
-    async close() {
-      await page.close();
-      await browser.close();
-    },
-  };
-};
+      await this.instance.page.setRequestInterception(true);
+
+      this.instance.page.on("request", (request) => {
+        const url = request.url();
+        if (blocked_domains.some((domain) => url.includes(domain)))
+          request.abort();
+        else request.continue();
+      });
+    }
+
+    return this.instance;
+  }
+
+  public static async anime(id: number) {
+    return anime(this.instance!.page, id);
+  }
+
+  public static async character(id: number) {
+    return character(this.instance!.page, id);
+  }
+
+  public static async person(id: number) {
+    return person(this.instance!.page, id);
+  }
+
+  public static async close() {
+    await this.instance!.page.close();
+    await this.instance!.browser.close();
+  }
+}
 
 export default MalWebScraper;
 export * from "./types";
