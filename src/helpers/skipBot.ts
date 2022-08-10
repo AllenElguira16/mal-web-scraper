013c2c116c96f48a64a78f5c029ccc7b1f5de336
Allen Elguira <AllenElguira16@gmail.com>
2022-08-10 09:21:20 +0800

    Add timeout

--- src/helpers/skipBot.ts
+++ src/helpers/skipBot.ts
@@ -8,12 +8,12 @@ export const skipBot = async (page: Page) => {
   if (isPageCaptcha) {
     await page.click('.g-recaptcha[data-action="submit"]');
 
-    await page.waitForNavigation({ waitUntil: "networkidle0" });
+    await page.waitForNavigation({ waitUntil: "networkidle2", timeout: 50000 });
 
     await page.click('.g-recaptcha[data-action="submit"]');
 
-    await page.waitForNavigation({ waitUntil: "networkidle0" });
+    await page.waitForNavigation({ waitUntil: "networkidle2", timeout: 50000 });
 
-    await page.reload();
+    await page.reload({ waitUntil: "networkidle2", timeout: 50000 });
   }
 };
